c0136bb228459c35abc50eb6dcb6454601a4715c
Shounak Das <155724860+Shoun1@users.noreply.github.com>
2025-08-08 21:35:51 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -3,26 +3,26 @@ import seaborn as sns
 import numpy as np
 from sklearn.metrics import confusion_matrix
 #test case 1
-'''y_true = ['Y','Y','Y','Y']
-y_pred = ['N','N','N','N']
+y_true1 = ['Y','Y','Y','Y']
+y_pred1 = ['N','N','N','N']
 
 #test case 2
-y_true = ['Y','N','Y','Y']
-y_pred = ['N','N','N','N']
+y_true2 = ['Y','N','Y','Y']
+y_pred2 = ['N','N','N','N']
 
 #test case 3
-y_true = ['Y','N','N','Y']
-y_pred = ['N','N','N','N']'''
+y_true3 = ['Y','N','N','Y']
+y_pred3 = ['N','N','N','N']
 
-y_true = ['Y','N','N','N']
-y_pred = ['N','N','N','N']
+y_true4 = ['Y','N','N','N']
+y_pred4 = ['N','N','N','N']
 
 
 print("Confusion Matrix:")
-print(confusion_matrix(y_true, y_pred))
-cm = confusion_matrix(y_true, y_pred)
+print(confusion_matrix(y_true1, y_pred1))
+cm = confusion_matrix(y_true1, y_pred1)
 
-def plot_confusion_matrix(y_true, y_pred):
+def plot_confusion_matrix(y_true1, y_pred1):
     cm = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Predicted 0', 'Predicted 1'], yticklabels=['True 0', 'True 1'])
